805d09cf2ec19c3f70f5dc62afa6b03ebc20b345
m081779 <marcoprincipio@hotmail.com>
2018-03-16 18:54:07 -0400

    trying to get deployment to work

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ToDo List
 Simple full stack To do list app for practice with Node, Express, MongoDB, and Handlebars
 
+<!-- [Click Here]() to view the dep -->
+
 
--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const taskController = require('../controller/taskController');
 
-router
-.route('/task/:id')
-.get(taskController.findOne)
-.put(taskController.complete)
-.delete(taskController.deleteOne)
+// router
+// .route('/task/:id')
+// .get(taskController.findOne)
+// .put(taskController.complete)
+// .delete(taskController.deleteOne)
 
-// router.get('/edit/:id', taskController.findOne);
+router.get('/edit/:id', taskController.findOne);
 
-// router.put('/complete/:id', taskController.complete);
+router.put('/complete/:id', taskController.complete);
 
-// router.delete('/delete/:id', taskController.deleteOne);
+router.delete('/delete/:id', taskController.deleteOne);
 
 router.post('/create', taskController.create);
 
