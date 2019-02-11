529f20aa7d22a0dee1710ab9436d7401f802f4d5
m081779 <marcoprincipio@hotmail.com>
2019-02-10 20:36:32 -0500

    added sorting of tasks alphabetically

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,17 +1,29 @@
 const Task = require('../models/task');
 
+const sortTask = (a,b) => {
+  const taskA = a.task.toLowerCase();
+  const taskB = b.task.toLowerCase();
+  return (taskA < taskB) ? -1 : (taskA > taskB) ? 1 : 0;
+}
+
 module.exports = {
   findAll: function (req,res){
     Task
     .find({})
-    .then(result =>  res.render('index', {tasks: result}))
+    .then(result => {
+      result.sort(sortTask)
+      res.render('index', {tasks: result})
+    })
     .catch(err => res.json(err))
   },
 
   create: function(req,res){
     Task
     .create(req.body)
-    .then(result => res.json(result))
+    .then(result => {
+      // result.sort(sortTask)
+      res.json(result)
+    })
     .catch(err => res.json(err));
   },
 
